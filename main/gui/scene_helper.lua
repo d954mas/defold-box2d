6c7bc246b7a8c2bdf74aaf7ef37ba112a80e49de
d954mas <d954mas@gmail.com>
2021-07-16 13:24:18 +0300

    add base ui

--- main/gui/scene_helper.lua
+++ main/gui/scene_helper.lua
@@ -20,10 +20,8 @@ M.world_step_time = 0
 
 M.debug_draw = {
     draw = BOX2D_UTILS.create_debug_draw(0.04),
-    flags = bit.bor(box2d.b2Draw.e_shapeBit,box2d.b2Draw.e_jointBit)
+    flags = bit.bor(box2d.b2Draw.e_shapeBit, box2d.b2Draw.e_jointBit)
 }
-
-M.debug_draw.draw:SetFlags(M.debug_draw.flags)
 
 function M.scene_new(cfg)
     M.dirty = true
@@ -41,7 +39,7 @@ function M.update(dt)
     local cfg = M.scene_config
     if (cfg.world) then
         local time = socket.gettime()
-        cfg.world:Step(cfg.dt * cfg.time_scale,cfg.velocityIterations,cfg.positionIterations)
+        cfg.world:Step(cfg.dt * cfg.time_scale, cfg.velocityIterations, cfg.positionIterations)
         M.world_step_time = socket.gettime() - time
         cfg.world:DebugDraw()
     end
@@ -52,14 +50,25 @@ function M.reset()
     M.dirty = true
     cfg.velocityIterations = 8
     cfg.positionIterations = 3
+    M.debug_draw.flags =  bit.bor(box2d.b2Draw.e_shapeBit, box2d.b2Draw.e_jointBit)
+    M.debug_draw.draw:SetFlags(M.debug_draw.flags)
 end
-
 
 function M.cfg_velocity_iterations_add(value)
-    M.scene_config.velocityIterations = LUME.clamp(M.scene_config.velocityIterations+value,1,100)
+    M.scene_config.velocityIterations = LUME.clamp(M.scene_config.velocityIterations + value, 1, 100)
 end
 function M.cfg_position_iterations_add(value)
-    M.scene_config.positionIterations = LUME.clamp(M.scene_config.positionIterations+value,1,100)
+    M.scene_config.positionIterations = LUME.clamp(M.scene_config.positionIterations + value, 1, 100)
+end
+
+function M.debug_draw_add_flag(flag)
+    M.debug_draw.flags = bit.bor(M.debug_draw.flags, flag)
+    M.debug_draw.draw:SetFlags(M.debug_draw.flags)
+end
+
+function M.debug_draw_remove_flag(flag)
+    M.debug_draw.flags = bit.band(M.debug_draw.flags, bit.bnot(flag))
+    M.debug_draw.draw:SetFlags(M.debug_draw.flags)
 end
 
 M.reset()
